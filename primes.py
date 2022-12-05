4b307fcfadd848f1d2e9f840ef85d0221b96894f
Roland Xhama <rxhama02@gmail.com>
2022-12-05 05:24:09 +0000

    Add same solution with exception handling

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f'Argument: {number_of_primes} cannot be 0 or negative!')
     list = []
+    num = 2
+    while(len(list) < number_of_primes):
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
